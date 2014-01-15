06edf9203996c2d32b5993cfb346d01d4f57eafd
Renat Bilalov <r.bilalov@xsolla.com>
2014-01-15 17:51:49 +0600

    phpunit config file

---
+++ tests/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Xsolla\SDK\Tests;
+
+
+use Xsolla\SDK\User;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    public function test() {
+        $user = new User('v1', 'v2', 'v3', 'email', 'phone');
+
+        $this->assertEquals('v1', $user->getV1());
+        $this->assertEquals('v2', $user->getV2());
+        $this->assertEquals('v3', $user->getV3());
+        $this->assertEquals('email', $user->getEmail());
+        $this->assertEquals('phone', $user->getPhone());
+    }
+}
+ 
---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+include_once __DIR__.'/../vendor/autoload.php';
