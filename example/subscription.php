9d7fe005abaf0269eeeb4d76e629b8ed02c976c2
Renat Bilalov <renatbilalov@gmail.com>
2014-01-24 12:16:57 +0600

    Fix subscription example

--- example/subscription.php
+++ example/subscription.php
@@ -11,17 +11,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 $user = new User('v1', 'v2');
 $subscription = new Subscriptions(new Client('https://api.xsolla.com'), new Project());
 
-/**
- * Поиск подписок
- */
 $userSubscriptions = $subscription->search($user, Subscriptions::TYPE_CARD);
-/**
- * Платеж на 100 голдов с сохраненной карты
- */
+
 $invoice = new Invoice(100);
 $subscription->pay($userSubscriptions[0], $invoice);
 
-/**
- * Удалить карту
- */
-$subscription->delete($userSubscriptions[0]['subscription_id'], Subscriptions::TYPE_CARD);
+$subscription->delete($userSubscriptions[0]);
