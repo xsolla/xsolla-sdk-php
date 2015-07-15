2e5da331f24ca04d8782693f730fd7abf0158aa1
Vitaliy Zakharov <v.zakharov@xsolla.com>
2015-07-15 22:33:31 +0500

    [WIP] CouponsTest

---
+++ tests/Integration/API/AbstractAPITest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Xsolla\SDK\Tests\Integration\API;
+
+use Xsolla\SDK\API\XsollaClient;
+
+class AbstractAPITest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var XsollaClient
+     */
+    protected $xsollaClient;
+
+    protected $projectId;
+
+    public function setUp()
+    {
+        $this->projectId = (int) $_SERVER['PROJECT_ID'];
+        $this->xsollaClient = XsollaClient::factory([
+            'merchant_id' => $_SERVER['MERCHANT_ID'],
+            'api_key' => $_SERVER['API_KEY']
+        ]);
+    }
+}
---
+++ tests/Integration/API/CouponsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Xsolla\SDK\Tests\Integration\API;
+
+class CouponsTest extends AbstractAPITest
+{
+    protected $code = '1wpb1igjBig0g';
+
+    protected $expectedCouponData = array (
+        'coupon_id' => 20886988,
+        'project_id' => 15861,
+        'coupon_code' => '1wpb1igjBig0g',
+        'campaign_code' => 'xsolla-sdk-php',
+        'virtual_currency_amount' => 1,
+        'is_active' => 1,
+        'redeems_count_remain' => NULL,
+        'redeems_count_for_user' => NULL,
+        'expiration_date' => NULL,
+        'virtual_items' => array (),
+    );
+
+    public function testGetCoupon()
+    {
+        $actualCouponData = $this->xsollaClient->GetCoupon(array(
+            'project_id' => $this->projectId,
+            'code' => $this->code,
+        ));
+        static::assertEquals($this->expectedCouponData, $actualCouponData);
+    }
+
+    public function testRedeemCoupon()
+    {
+        $actualCouponData = $this->xsollaClient->RedeemCoupon(array(
+            'project_id' => $this->projectId,
+            'code' => $this->code,
+            'user_id' => 1,
+        ));
+        static::assertEquals($this->expectedCouponData, $actualCouponData);
+    }
+}
