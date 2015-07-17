bfecbf783ed39eed772f99e362c52951fa9900a1
Vitaliy Zakharov <v.zakharov@xsolla.com>
2015-07-17 11:41:40 +0500

    [WIP] PromotionsTest

--- tests/Integration/API/PromotionsTest.php
+++ tests/Integration/API/PromotionsTest.php
@@ -7,34 +7,23 @@ namespace Xsolla\SDK\Tests\Integration\API;
  */
 class PromotionsTest extends AbstractAPITest
 {
-    public function testCreatePromotion()
-    {
-        static::markTestSkipped();
-    }
+    protected static $promotionId;
 
-    public function testGetPromotion()
-    {
-        static::markTestSkipped();
-    }
+    protected $promotion;
 
-    public function testUpdatePromotion()
+    public function setUp()
     {
-        static::markTestSkipped();
-    }
-
-    public function testReviewPromotion()
-    {
-        static::markTestSkipped();
-    }
-
-    public function testTogglePromotion()
-    {
-        static::markTestSkipped();
-    }
-
-    public function testDeletePromotion()
-    {
-        static::markTestSkipped();
+        parent::setUp();
+        $this->promotion = array(
+            'technical_name' => uniqid('promotion_'),
+            'name' => array(
+                'en' => 'name'
+            ),
+            'description' => array(
+                'en' => 'description'
+            ),
+            'project_id' => $this->projectId,
+        );
     }
 
     public function testListPromotions()
@@ -43,43 +32,172 @@ class PromotionsTest extends AbstractAPITest
         static::assertInternalType('array', $response);
     }
 
-    public function testGetPromotionSubject()
+    public function testCreatePromotion()
     {
-        static::markTestSkipped();
+        $response = $this->xsollaClient->CreatePromotion(array(
+            'request' => $this->promotion,
+        ));
+        static::assertArrayHasKey('id', $response);
+        static::$promotionId = $response['id'];
     }
 
+    /**
+     * @depends testCreatePromotion
+     */
+    public function testGetPromotion()
+    {
+        $response = $this->xsollaClient->GetPromotion(array(
+            'promotion_id' => static::$promotionId,
+        ));
+        static::assertInternalType('array', $response);
+    }
+
+    /**
+     * @depends testGetPromotion
+     */
+    public function testUpdatePromotion()
+    {
+        $this->xsollaClient->UpdatePromotion(array(
+            'promotion_id' => static::$promotionId,
+            'request' => $this->promotion,
+        ));
+    }
+
+    /**
+     * @depends testUpdatePromotion
+     */
     public function testSetPromotionSubject()
     {
-        static::markTestSkipped();
+        $this->xsollaClient->SetPromotionSubject(array(
+            'promotion_id' => static::$promotionId,
+            'request' => array(
+                'purchase' => false,
+                'items' => null,
+                'packages' => array(1),
+            ),
+        ));
     }
 
-    public function testGetPromotionPaymentSystems()
+    /**
+     * @depends testSetPromotionSubject
+     */
+    public function testGetPromotionSubject()
     {
-        static::markTestSkipped();
+        $response = $this->xsollaClient->GetPromotionSubject(array(
+            'promotion_id' => static::$promotionId,
+        ));
+        static::assertInternalType('array', $response);
     }
 
+    /**
+     * @depends testGetPromotionSubject
+     */
     public function testSetPromotionPaymentSystems()
     {
-        static::markTestSkipped();
+        return;//TODO
+        $this->xsollaClient->SetPromotionPaymentSystems(array(
+            'promotion_id' => static::$promotionId,
+            'request' => array(
+                array('id' => 1),
+            ),
+        ));
     }
 
-    public function testGetPromotionPeriods()
+    /**
+     * @depends testSetPromotionPaymentSystems
+     */
+    public function testGetPromotionPaymentSystems()
     {
-        static::markTestSkipped();
+        $response = $this->xsollaClient->GetPromotionPaymentSystems(array(
+            'promotion_id' => static::$promotionId,
+        ));
+        static::assertInternalType('array', $response);
     }
 
+    /**
+     * @depends testGetPromotionPaymentSystems
+     */
     public function testSetPromotionPeriods()
     {
-        static::markTestSkipped();
+        $dateTime = new \DateTime('next month');
+        $this->xsollaClient->SetPromotionPeriods(array(
+            'promotion_id' => static::$promotionId,
+            'request' => array(
+                'periods' => array(
+                    array(
+                        'from' => $dateTime->format(\DateTime::ISO8601),
+                        'to' => $dateTime->modify('+ 1 month')->format(\DateTime::ISO8601),
+                    ),
+                )
+            ),
+        ));
     }
 
-    public function testGetPromotionRewards()
+    /**
+     * @depends testSetPromotionPeriods
+     */
+    public function testGetPromotionPeriods()
     {
-        static::markTestSkipped();
+        $response = $this->xsollaClient->GetPromotionPeriods(array(
+            'promotion_id' => static::$promotionId,
+        ));
+        static::assertInternalType('array', $response);
     }
 
+    /**
+     * @depends testGetPromotionPeriods
+     */
     public function testSetPromotionRewards()
     {
-        static::markTestSkipped();
+        $this->xsollaClient->SetPromotionRewards(array(
+            'promotion_id' => static::$promotionId,
+            'request' => array(
+                'purchase' => array(
+                    'discount_percent' => 10,
+                )
+            ),
+        ));
+    }
+
+    /**
+     * @depends testSetPromotionRewards
+     */
+    public function testGetPromotionRewards()
+    {
+        $response = $this->xsollaClient->GetPromotionRewards(array(
+            'promotion_id' => static::$promotionId,
+        ));
+        static::assertInternalType('array', $response);
+    }
+
+    /**
+     * @depends testGetPromotionRewards
+     */
+    public function testReviewPromotion()
+    {
+        $response = $this->xsollaClient->ReviewPromotion(array(
+            'promotion_id' => static::$promotionId,
+        ));
+        static::assertInternalType('array', $response);
+    }
+
+    /**
+     * @depends testReviewPromotion
+     */
+    public function testTogglePromotion()
+    {
+        $this->xsollaClient->TogglePromotion(array(
+            'promotion_id' => static::$promotionId,
+        ));
+    }
+
+    /**
+     * @depends testTogglePromotion
+     */
+    public function testDeletePromotion()
+    {
+        $this->xsollaClient->DeletePromotion(array(
+            'promotion_id' => static::$promotionId,
+        ));
     }
 }
