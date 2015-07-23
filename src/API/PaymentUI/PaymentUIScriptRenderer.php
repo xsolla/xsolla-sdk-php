94bda4e4f88f560ca9537cf07b84f9fb7f84cde2
Vitaliy Zakharov <v.zakharov@xsolla.com>
2015-07-23 14:02:20 +0500

    [WIP] PaymentUIScriptRenderer fix [ci skip]

--- src/API/PaymentUI/PaymentUIScriptRenderer.php
+++ src/API/PaymentUI/PaymentUIScriptRenderer.php
@@ -17,20 +17,20 @@ class PaymentUIScriptRenderer
         $template =
 <<<EOF
 <script>
-     var options = {
-         access_token: '%s',
-         sandbox: %s
-     };
-     var s = document.createElement('script');
-     s.type = "text/javascript";
-     s.async = true;
-     s.src = "https://xsolla.cachefly.net/embed/paystation/1.0.1/widget.min.js";
-     s.addEventListener('load', function (e) {
-         XPayStationWidget.init(options);
-     }, false);
-     var head = document.getElementsByTagName('head')[0];
-     head.appendChild(s);
- </script>
+    var options = {
+        access_token: '%s',
+        sandbox: %s
+    };
+    var s = document.createElement('script');
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "//static.xsolla.com/embed/paystation/1.0.2/widget.min.js";
+    s.addEventListener('load', function (e) {
+        XPayStationWidget.init(options);
+    }, false);
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(s);
+</script>
 EOF;
         return sprintf($template, $token, $isSandbox ? 'true' : 'false');
     }
